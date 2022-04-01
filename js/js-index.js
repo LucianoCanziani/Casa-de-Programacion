f5809bfb99ecb3b723a95f6452e14e76a4b51544
Luciano Canziani <96893895+LucianoCanziani@users.noreply.github.com>
2022-04-01 13:33:36 -0300

    Add files via upload

--- js/js-index.js
+++ js/js-index.js
@@ -1,81 +1,90 @@
-$(function () {
-
-////////////////// EVENTOS QUE LLEVAN AL USUARIO A SECCIONES DE LA LANDING PAGE
-
-  $(".cursoNav").click(function () { 
-    
-    $('html,body').animate({
-      scrollTop:$('#cursos').offset().top
-    },300);
-
-  });
-
-  $(".estudiantesNav").click(function () { 
-    
-    $('html,body').animate({
-      scrollTop:$('.students').offset().top
-    },300);
-
-  });
-
-////////////////// SLICK SLIDER PROFESORES
-
-  $('.expertosIndustria').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    arrows: true,
-    infinite: true,
-    centerMode: true,
-    centerPadding: '0px',
-    variableWidth: true,
-    speed: 1000,
-    autoplay: true,
-    autoplaySpeed: 5000,
-  });
+$(function() {
 
 
-////////////////// ELECCION DEL CURSO POR EVENTO, GUARDA EN LOCALSTORAGE Y PROCEDE A LA SIGUIENTE PAGINA
 
-  $('.info-curso').on('click', function (e) {
-    
-    let curso = {
-      'precio': $(e.currentTarget).find('input[name=precio]').val(),
-      'curso': $(e.currentTarget).find('input[name=cursoNombre]').val()
-    };
+    ////////////////// EVENTOS QUE LLEVAN AL USUARIO A SECCIONES DE LA LANDING PAGE
 
-    localStorage.setItem('datosCurso', JSON.stringify(curso));
+    $(".cursoNav").click(function() {
 
-    localStorage.setItem('datosCursoMostrar', JSON.stringify(curso));
+        $('html,body').animate({
+            scrollTop: $('#cursos').offset().top
+        }, 300);
 
-    window.location.href = "carrito/fechas.html";
+    });
 
-    e.preventDefault()
-  });
+    $(".estudiantesNav").click(function() {
 
-////////////////// SLICK SLIDER ESTUDIANTES
+        $('html,body').animate({
+            scrollTop: $('.students').offset().top
+        }, 300);
 
-$('.testimonios').slick({
-  dots: false,
-  infinite: true,
-  arrows: true,
-  slidesToShow: 2,
-  slidesToScroll: 1,
-  speed: 1000,
-});
+    });
 
-////////////////// PREGUNTAS FRECUENTES 
+    ////////////////// SLICK SLIDER PROFESORES
 
-  $(".pregunta").on('click', function (e) {
+    $('.expertosIndustria').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        infinite: true,
+        centerMode: true,
+        centerPadding: '0px',
+        variableWidth: true,
+        speed: 1000,
+        autoplay: true,
+        autoplaySpeed: 5000,
+    });
 
-    $(e.currentTarget).children('.respuesta').slideToggle();
-    
-  /////// ORIENTACION DE LA FLECHA DE PREGUNTAS FRECUENTES
 
-    let isOpening = !$(this).hasClass('opened');
+    ////////////////// ELECCION DEL CURSO POR EVENTO, GUARDA EN LOCALSTORAGE Y PROCEDE A LA SIGUIENTE PAGINA
 
-    $(this).find('.titulo-arrow .arrow').css('transform', isOpening ? 'rotate(180deg)' : 'none');
-       if (isOpening) $(this).addClass('opened');
-       else $(this).removeClass('opened');
-    
-  });
+    $('.info-curso').on('click', function(e) {
+
+        let curso = {
+            'precio': $(e.currentTarget).find('input[name=precio]').val(),
+            'curso': $(e.currentTarget).find('input[name=cursoNombre]').val()
+        };
+
+        localStorage.setItem('datosCurso', JSON.stringify(curso));
+
+        localStorage.setItem('datosCursoMostrar', JSON.stringify(curso));
+
+        window.location.href = "carrito/fechas.html";
+
+        e.preventDefault()
+    });
+
+    ////////////////// SLICK SLIDER ESTUDIANTES
+
+    $('.testimonios').slick({
+        dots: false,
+        infinite: true,
+        arrows: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        speed: 1000,
+    });
+
+    ////////////////// PREGUNTAS FRECUENTES 
+
+    $(".pregunta").on('click', function(e) {
+
+        $(e.currentTarget).children('.respuesta').show();
+
+
+    });
+/*
+    $(".pregunta").on('click', function(e) {
+
+        $(e.currentTarget).children('.respuesta').show();
+
+        /////// ORIENTACION DE LA FLECHA DE PREGUNTAS FRECUENTES
+
+        let isOpening = !$(this).hasClass('luccan-opened');
+
+        $(this).find('.titulo-arrow .arrow').css('transform', isOpening ? 'rotate(180deg)' : 'none');
+        if (isOpening) $(this).addClass('luccan-opened');
+        else $(this).removeClass('luccan-opened');
+
+    });*/
 });
